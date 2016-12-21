b62faa8bbf5dc45cf36fdf59fe814be0d37bf565
Stefan Wollny <sw@secomba.com>
2016-12-21 17:03:34 +0100

    Support other reporters using queryVariable reporter

--- start.js
+++ start.js
@@ -1,7 +1,29 @@
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    return null;
+}
 process.nextTick(function() {
 	delete require.cache[module.id];
 	if(typeof window !== "undefined" && window.mochaPhantomJS)
 		mochaPhantomJS.run();
-	else
+	else {
 		mocha.run();
+        var reporter = getQueryVariable('reporter');
+        if (reporter) {
+            mocha.reporter(reporter);
+        }
+        runner = mocha.run();
+        runner.on('end', function() {
+            if (reporter === 'json') {
+                document.querySelector('#mocha').innerText = JSON.stringify(runner.testResults);
+            }
+        });
+    }
 });
